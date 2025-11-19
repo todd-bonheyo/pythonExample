a0ab38091b393943fb3d17f64ba595407381c579
todd-bonheyo <todd.bonheyo@gallaudet.edu>
2025-11-18 19:51:39 -0500

    Add files via upload

---
+++ 11.3 - vending machines.py
@@ -0,0 +1,26 @@
+class VendingMachine:
+    def __init__(self, num):
+        self.count = num
+        self.max = num
+
+    def refill(self): 
+        self.count = self.max
+        print('Refilled')
+    def sell(self, order):
+        self.count -= order
+        print('Sold:', order)
+    def print_stock(self): 
+        print('Current stock:', self.count)
+
+    def __str__(self):
+        return "Give me my coffee"
+
+stock = int(input("Enter current stock: "))
+vm = VendingMachine(stock)
+vm.print_stock()
+order_1 = int(input("Enter order: "))
+vm.sell(order_1)
+vm.print_stock()
+vm.refill()
+vm.print_stock()
+print(vm)
